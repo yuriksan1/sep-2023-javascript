f8d613c8048258266a8150abc7af09af6aaac0bd
yuriksan1 <yurakomow2@gmail.com>
2023-11-21 17:43:48 +0200

    Homework1

---
+++ lesson1/Homework1/main.js2.js
@@ -0,0 +1,75 @@
+let s1;
+s1="hello";
+console.log(s1);
+
+let s2;
+s2="owu";
+console.log(s2);
+
+let s3;
+s3="com";
+console.log(s3);
+
+let s4;
+s4 = "ua";
+console.log(s4);
+
+let n1;
+n1=1;
+console.log(n1);
+
+let n2;
+n2= 10;
+console.log(n2);
+
+let n3;
+n3 = -999;
+console.log(n3);
+
+let n4;
+n4=123;
+console.log(n4);
+
+let n5;
+n5=3.14;
+console.log(n5);
+
+let n6;
+n6=2.7;
+console.log(n6);
+
+let n7;
+n7=16;
+console.log(n7);
+
+let b1;
+b1=true;
+console.log(b1);
+
+let b2;
+b2=false;
+console.log(b2);
+
+//---------------------------------------------------
+let firstName;
+firstName="Yura";
+
+let middleName;
+middleName="Komov";
+
+let lastName;
+lastName="Oleksandrovich";
+
+let msg = firstName + " " + middleName + " " + lastName;
+console.log(msg);
+//------------------------------------------------------
+let a = 100;
+console.log(typeof a);
+
+let b = "100";
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+//------------------------------------------------------
+
---
+++ lesson1/scripts/main.js
@@ -0,0 +1,87 @@
+// //number
+// let n1;
+// n1=123.1323;
+// //string
+// let s1 = "hello Yura";
+// s1 = "qwewrwer";
+// //boolean
+//
+// // > < >= <= == != === !==
+// let b1 = 5 > 6 ;
+// console.log(b1);
+//
+// let bool = 5 == "5"; //"5" == "5" -> 0091 == 0091
+// console.log(bool);
+//
+// bool = 100 === "100";
+// console.log(bool);
+
+// let x = null;
+// console.log(typeof x);
+//
+// //number
+// //string
+// //boolean
+// //undefined
+// //null
+// //NaN
+// //Symbol
+// //object [] {}
+//
+// console.log(typeof NaN);
+// console.log(100/"adqw")
+
+// let a = 10;
+// let b = 20;
+//
+// console.log(a+b);
+//
+// console.log(10%3);
+//
+// let str1="hello ";
+// let str2="Yura";
+// console.log(str1 + str2);
+// console.log(`${str1} ${str2} !!!`)
+//
+// console.log(str1-str2);
+//
+// let ss1="100";
+// let ss2="200";
+// console.log(ss1-ss2);
+// console.log(ss1 + ss2);
+
+// //number to string
+// let x =100;
+// console.log(x+"");
+// console.log(300 + "");
+// console.log("" + 100 + 200);
+
+//String to Number
+// let ns = "100500";
+// let nn = +ns;
+// console.log(nn);
+// let number = parseInt("100500adawd")
+// console.log(number);
+
+console.log(true + true);
+console.log(true + false);
+
+console.log(100 + true);
+console.log("100" / true);
+
+//boolean to namber
+console.log(+true);
+console.log(+false);
+
+//number to boolean
+console.log(!!100);
+console.log(!!-100);
+console.log(!!"dadad");
+console.log("---------");
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!-Infinity);
+console.log(!!Infinity);
