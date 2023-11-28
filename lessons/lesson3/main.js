9d245907eab3e7ed00fc0b1c0aea9fe21be33378
yuriksan1 <yurakomow2@gmail.com>
2023-11-28 15:34:16 +0200

    Homework1

---
+++ lessons/lesson3/main.js
@@ -0,0 +1,40 @@
+// let a = 0;
+// a = a + 1; // a = 0 + 1 = 1
+// a += 1; //a = 1 + 1 = 2
+// a++;//3
+// ++a;//4
+//
+// let x = 0;
+// let y = x++;
+// console.log(x,y);
+
+// debugger;
+// for (let i = 0;i<10; i++){
+//  console.log(i);
+// }
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+console.log(users[0])
+console.log(users[1])
+console.log(users[2])
+console.log(users[3])
+console.log(users[4])
+console.log(users[5])
+console.log(users[6])
+console.log(users[7])
+console.log(users[8])
+console.log(users[9])
+console.log(users[10])
