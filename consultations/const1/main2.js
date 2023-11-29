0246990d3c051493ad0618c8b074205708f610a4
yuriksan1 <yurakomow2@gmail.com>
2023-11-29 18:19:28 +0200

    Homework3

---
+++ consultations/const1/main2.js
@@ -0,0 +1,66 @@
+console.log("TEST")
+let cars = [
+    {make: "Toyota", model: "Camry", year: 2022, color: "Blue", price: 25000},
+    {make: "Honda", model: "Civic", year: 2021, color: "Red", price: 22000},
+    {make: "Ford", model: "Mustang", year: 2023, color: "Black", price: 40000},
+    {make: "Chevrolet", model: "Silverado", year: 2022, color: "Gray", price: 35000},
+    {make: "Nissan", model: "Altima", year: 2021, color: "White", price: 23000},
+    {make: "BMW", model: "X5", year: 2022, color: "Silver", price: 60000},
+    {make: "Mercedes-Benz", model: "C-Class", year: 2023, color: "Black", price: 50000},
+    {make: "Audi", model: "A4", year: 2022, color: "Blue", price: 45000},
+    {make: "Jeep", model: "Wrangler", year: 2022, color: "Green", price: 35000},
+    {make: "Lexus", model: "RX", year: 2023, color: "Champagne", price: 55000}
+];
+
+// for (const car of cars){
+//    if (car.price >= 25000){
+//        console.log(car)
+//    }
+// }
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+//
+// for (let i = numbers.length-1; i >= 0; i--) {
+//     const number = numbers[i];
+//     console.log(number)
+// }
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,1212121];
+//
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i];
+//     console.log(numbers)
+//
+// }
+
+let numbers = [1,2,3,4,5,6,7,8,9,10,131232];
+
+// for (let i = 0;i < numbers.length;i++){
+//     const number = numbers[i];
+// }
+let even = [];
+let noEven = [];
+
+for (let i = 0; i < numbers.length; i++) {
+console.log("Index",i);
+console.log("Znac:",numbers[i]);
+
+//     const elem = numbers[i];
+//     // console.log(elem);
+//     if (elem % 2 === 0) {
+//         even[even.length] = elem;
+//     } else {
+//         noEven[noEven.length] = elem;
+//     }
+}
+//
+// console.log(even);
+// console.log(noEven);
+
+// document.write(`<ul>`)
+
+// for (const num of numbers){
+//     document.write(`<div class="box"><h2>${num}</h2></div>`)
+// }
+
+// document.write(`</ul>`)
--- lessons/lesson3/main.js
+++ lessons/lesson3/main.js
@@ -13,28 +13,118 @@
 //  console.log(i);
 // }
 
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+
+// console.log(users[0])
+// console.log(users[1])
+// console.log(users[2])
+// console.log(users[3])
+// console.log(users[4])
+// console.log(users[5])
+// console.log(users[6])
+// console.log(users[7])
+// console.log(users[8])
+// console.log(users[9])
+// console.log(users[10])
+
+// for (let i = 0;i<users.length; i++){
+// console.log(users[i])
+// }
+
+// let users = [
+//     {skills: ['html', 'js', 'java'], name: 'vasya', age: 31, status: false},
+//     {skills: ['html', 'js', 'C#'], name: 'petya', age: 30, status: true},
+//     {skills: ['html', 'js'], name: 'kolya', age: 29, status: true},
+//     {skills: ['html', 'js', 'node'], name: 'olya', age: 28, status: false},
+//     {skills: ['html', 'js'], name: 'max', age: 30, status: true},
+//     {skills: ['html', 'js', 'react'], name: 'anya', age: 31, status: false},
+//     {skills: ['html', 'js', 'react', 'node'], name: 'oleg', age: 28, status: false},
+//     {skills: ['html', 'js', 'react', 'node'], name: 'andrey', age: 29, status: true},
+//     {skills: ['html', 'js', 'react', 'node'], name: 'masha', age: 30, status: true},
+//     {skills: ['html', 'js', 'react', 'node'], name: 'olya', age: 31, status: false},
+//     {skills: ['html', 'js', 'react', 'node'], name: 'max', age: 31, status: true}
+// ];
+
+// for (let i = 0; i < users.length; i++) {
+//     const user = users[i];
+//     if (!user.status){
+// console.log(user);
+//     }
+// }
+
+//
+
+// const x = [];
+// //x = [1,2,3]; error
+// x[0] = "wrrw";
+// console.log(x)
+
+// for (const user of users){
+//     let skills = user.skills;
+//     for (const skill of skills) {
+//         console.log(skill);
+//     }
+//     console.log("------------");
+// }
+
+// for (let minutes = 0; minutes < 2; minutes++) {
+//     for (let seconds = 0; seconds < 60; seconds++) {
+//         console.log(minutes, seconds);
+//     }
+// }
+
+// let car = {id:1,model:"subaru",power:234,status:true,parts:[111,222,333]};
+//
+// // console.log(car["id"]);
+// // console.log(car["model"]);
+// for (let carKey in car) {
+//     console.log(carKey,car[carKey])
+// }
+
 let users = [
     {name: 'vasya', age: 31, status: false},
     {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
+    {name: 'kolya', age: 29, status: true,qwe:'kjsghsjgfdsj'},
     {name: 'olya', age: 28, status: false},
     {name: 'max', age: 30, status: true},
     {name: 'anya', age: 31, status: false},
     {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
+    {name: 'andrey', age: 29, status: true,asd:'qwe'},
     {name: 'masha', age: 30, status: true},
     {name: 'olya', age: 31, status: false},
     {name: 'max', age: 31, status: true}
 ];
 
-console.log(users[0])
-console.log(users[1])
-console.log(users[2])
-console.log(users[3])
-console.log(users[4])
-console.log(users[5])
-console.log(users[6])
-console.log(users[7])
-console.log(users[8])
-console.log(users[9])
-console.log(users[10])
+// document.write(`<ul>`)
+//
+// for (const user of users) {
+//     document.write(`<li>`)
+//
+//     for (const userKey in user) {
+//         document.write(`${userKey} - ${user[userKey]} `);
+//     }
+//     document.write(`</li>`)
+// }
+// document.write(`</ul>`)
+
+let i = 0;
+while (i < users.length){
+    console.log(users[i]);
+    i++;
+}
+
+do {
+console.log(`wwqeqeq`)
+}while (false)
